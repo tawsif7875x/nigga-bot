988fa59e61c5052f974775c007859ae7bf9915b2
Tawsif <challengex46@gmail.com>
2025-03-01 18:34:37 +0600

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -28,31 +28,7 @@ module.exports = {
 	},
 
 	async execute({ api, args, message, event, threadsData, usersData, dashBoardData, globalData, threadModel, userModel, dashBoardModel, globalModel, role, commandName, getLang }) {
-		function output(msg) {
-			if (typeof msg == "number" || typeof msg == "boolean" || typeof msg == "function")
-				msg = msg.toString();
-			else if (msg instanceof Map) {
-				let text = `Map(${msg.size}) `;
-				text += JSON.stringify(mapToObj(msg), null, 2);
-				msg = text;
-			}
-			else if (typeof msg == "object")
-				msg = JSON.stringify(msg, null, 2);
-			else if (typeof msg == "undefined")
-				msg = "undefined";
-
-			message.reply(msg);
-		}
-		function out(msg) {
-			output(msg);
-		}
-		function mapToObj(map) {
-			const obj = {};
-			map.forEach(function (v, k) {
-				obj[k] = v;
-			});
-			return obj;
-		}
+		
 		const cmd = `
 		(async () => {
 			try {
@@ -61,11 +37,7 @@ module.exports = {
 			catch(err) {
 				log.err("eval command", err);
 				message.send(
-					"${getLang("error")}\\n" +
-					(err.stack ?
-						removeHomeDir(err.stack) :
-						removeHomeDir(JSON.stringify(err, null, 2) || "")
-					)
+					"${getLang("error")}"
 				);
 			}
 		})()`;
