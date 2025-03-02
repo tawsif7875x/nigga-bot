7cf696f25e50c5ab60b12e8ef0e8a95ee1dd84e8
Tawsif <challengex46@gmail.com>
2025-03-03 00:06:30 +0600

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -8,7 +8,7 @@ role: 0,
 category: "no prefix",
 guide: `prefix`
 },
-onLoad: async function({ api, event, args }) {
+onCall: async function({ api, event, args }) {
   const t = args[0];
 if (t === "prefix") {
 api.sendMessage(`🌐 System prefix: ${config.prefix}\n🛸 Your box chat prefix: ${config.prefix}`, event.threadID);
