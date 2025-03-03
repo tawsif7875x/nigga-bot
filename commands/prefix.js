921ed113b33c11f66d8099bed5e0623a293dccca
Tawsif <challengex46@gmail.com>
2025-03-03 23:12:45 +0600

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -9,9 +9,12 @@ category: "no prefix",
 guide: `prefix`
 },
 async function({ api, event, args }) {
+  try {
   const t = event.body;
 if (t.match(/prefix/)) {
 api.sendMessage(`🌐 System prefix: ${config.prefix}\n🛸 Your box chat prefix: ${config.prefix}`, event.threadID);
 }
+  } catch (error) { api.sendMessage(`${error.message}`, event.threadID);
+                  }
 }
 }
