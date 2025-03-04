ded99b24f8f1dc7a6fa0708ac03349c932bcda8b
Tawsif <challengex46@gmail.com>
2025-03-04 16:12:21 +0600

    Create unsend.js

---
+++ commands/unsend.js
@@ -0,0 +1,12 @@
+module.exports = {
+config: {
+	name: "uid",
+	author: "Tawsif~",
+	role: 0,
+},
+async execute({ api, event }) {
+const botID = api.getCurrentUserID();
+if (event.messageReply && event.messageReply.senderID === botID ) { api.unsendMessage(event.messageReply.messageID);
+} else { api.sendMessage("reply to the message you want to unsend", event.threadID);}
+	}
+}
