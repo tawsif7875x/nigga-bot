e8fa89cb080c25343c12a0110de670ba3c6ab1b2
Tawsif <challengex46@gmail.com>
2025-03-05 23:54:17 +0600

    Update upt.js

--- commands/upt.js
+++ commands/upt.js
@@ -12,8 +12,6 @@ const u = process.uptime();
 const h = Math.floor(u/3600);
 const m = Math.floor((u/60) % 60);
 const s = Math.floor(u % 60);
-const ms = (u % 1);
-
-api.sendMessage(`${h} hrs ${m} min ${s}sec ${ms} ms`, event.threadID);
+api.sendMessage(`${h} Hrs ${m} Min ${s} Sec`, event.threadID);
 }
 }
