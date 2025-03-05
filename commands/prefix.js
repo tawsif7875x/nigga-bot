3d3941411d81290d3c16a2485604d88eb5073207
Tawsif <challengex46@gmail.com>
2025-03-05 12:38:43 +0600

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,16 +1,16 @@
 const config = require('../config.json');
 module.exports = {
-config: {
-name: "prefix",
-aliases: [],
-author: "Tawsif~",
-role: 0,
-category: "no prefix",
-guide: "prefix"
-},
-onChat: async execute({ api, event, args }) {
-if (event.body.match(/prefix/)) {
-api.sendMessage(`🌐 System prefix: ${config.prefix}\n🛸 Your box chat prefix: ${config.prefix}`, event.threadID);
-}
-}
+  config: {
+    name: "prefix",
+    aliases: [],
+    author: "Tawsif~",
+    role: 0,
+    category: "no prefix",
+    guide: "prefix"
+  },
+  onLoad: async function execute({ api, event, args }) {
+    if (event.body.match(/prefix/)) {
+      api.sendMessage(`🌐 System prefix: ${config.prefix}\n🛸 Your box chat prefix: ${config.prefix}`, event.threadID);
+    }
+  }
 }
