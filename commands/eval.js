13d4f306f638d5e7a29e2a7b0fbc58b62ff5eaae
Tawsif <challengex46@gmail.com>
2025-03-05 12:34:45 +0600

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -1,45 +1,16 @@
+const config = require('../config.json');
 module.exports = {
-	config: {
-		name: "eval",
-		version: "1.6",
-		author: "NTKhangn & Tawsif~",
-		countDown: 5,
-		role: 2,
-		description: {
-			vi: "Test code nhanh",
-			en: "Test code quickly"
-		},
-		category: "owner",
-		guide: {
-			vi: "{pn} <đoạn code cần test>",
-			en: "{pn} <code to test>"
-		}
-	},
-
-	langs: {
-		vi: {
-			error: "❌ Đã có lỗi xảy ra:"
-		},
-		en: {
-			error: "❌ An error occurred:"
-		}
-	},
-
-	async execute({ api, args, event, Users, commands, Threads }) {
-		let a = args.join(" ");
-		if (a.match(/out/)) {
-			a = a.replace(/out/g, "api.sendMessage(`${") + "}`, event.threadID);";
-			} else { a = args.join(" ");
-			       }
-			const cmd = `
-			(async () => {
-				try {
-					${a}
-				} catch (error) {
-					api.sendMessage("${this.langs.en.error} " + error.message, event.threadID);
-				}
-			})()`;
-			eval(cmd);
-		
-	}
-};
+  config: {
+    name: "prefix",
+    aliases: [],
+    author: "Tawsif~",
+    role: 0,
+    category: "no prefix",
+    guide: "prefix"
+  },
+  onChat: async function execute({ api, event, args }) {
+    if (event.body.match(/prefix/)) {
+      api.sendMessage(`🌐 System prefix: ${config.prefix}\n🛸 Your box chat prefix: ${config.prefix}`, event.threadID);
+    }
+  }
+}
