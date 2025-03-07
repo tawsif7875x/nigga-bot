3fe8387f550e4b87a8ff0fce01c6cda2174cd304
Tawsif <challengex46@gmail.com>
2025-03-08 01:02:25 +0600

    Update xl.js

--- commands/xl.js
+++ commands/xl.js
@@ -17,13 +17,14 @@ try {
 const t = new Date().getTime();
 		const url = `https://tawsifs-xl.onrender.com/xl?prompt=${encodeURIComponent(prompt)}`;
 const response = await axios.get(url);
-const response2 = await axios.get(response.data.imageUrl, { responseType: 'arraybuffer'});
-const w = fs.writeFile("./xl.png", response2.data);
+	const imageUrl = response.data.imageUrl;
+const response2 = await axios.get(imageUrl, { responseType: 'arraybuffer'});
+const w = fs.writeFile('./xl.png', response2.data);
 
 const t2 = new Date().getTime();
 
 await api.sendMessage({ body: `✅ | Here's your image ✨\n🕔 | Time taken: ${(t2-t)/1e3} seconds`,
-attachment: fs.createReadStream('xl.png');
+attachment: fs.createReadStream('./xl.png')
 }, event.threadID, event.messageID);
 	api.setMessageReactionMqtt("✅", event.messageID, event.threadID);
 } catch (error) { api.sendMessage("❌ | " + error.message, event.threadID);
