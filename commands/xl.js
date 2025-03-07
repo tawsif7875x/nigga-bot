10204c8c35d1b8491ab6b5753138d392c0fc421f
Tawsif <challengex46@gmail.com>
2025-03-08 00:40:03 +0600

    Update xl.js

--- commands/xl.js
+++ commands/xl.js
@@ -16,7 +16,7 @@ try {
 const t = new Date().getTime();
 		const url = `https://tawsifs-xl.onrender.com/xl?prompt=${encodeURIComponent(prompt)}`;
 const response = await axios.get(url);
-const response2 = await axios.get(response.data.imageUrl, { responseType: 'stream' });
+const response2 = await axios.get(response.data.imageUrl, { responseType: 'stream', Content-Type: 'image/png'});
 const t2 = new Date().getTime();
 
 await api.sendMessage({ body: `✅ | Here's your image ✨\n🕔 | Time taken: ${(t2-t)/1e3} seconds`,
