3823533de0018c93b461214539308204c2e84084
Tawsif <challengex46@gmail.com>
2025-03-08 13:03:53 +0600

    Update xl.js

--- commands/xl.js
+++ commands/xl.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const fs = require('fs');
+const tinyurl = require('tinyurl');
 module.exports = {
 config: {
 	name: "xl",
@@ -18,12 +19,13 @@ const t = new Date().getTime();
 		const url = `https://tawsifs-xl.onrender.com/xl?prompt=${encodeURIComponent(prompt)}`;
 const response = await axios.get(url);
 	const imageUrl = response.data.imageUrl;
+	const img = await tinyurl.shorten(imageUrl);
 const response2 = await axios.get(imageUrl, { responseType: 'arraybuffer'});
 const w = fs.writeFileSync('./xl.png', Buffer.from(response2.data, 'binary'));
 
 const t2 = new Date().getTime();
 
-await api.sendMessage({ body: `✅ | Here's your image ✨\n🕔 | Time taken: ${(t2-t)/1e3} seconds`,
+await api.sendMessage({ body: `✅ | Here's your image ✨\n🕔 | Time taken: ${(t2-t)/1e3} seconds\nurl: ${img}`,
 attachment: fs.createReadStream('./xl.png')
 }, event.threadID, event.messageID);
 	api.setMessageReactionMqtt("✅", event.messageID, event.threadID);
