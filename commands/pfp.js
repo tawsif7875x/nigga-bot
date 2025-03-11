83c1f50e1ec9d08b75dda87810b35382c2d9e585
Tawsif <challengex46@gmail.com>
2025-03-11 16:01:47 +0600

    Update pfp.js

--- commands/pfp.js
+++ commands/pfp.js
@@ -15,7 +15,7 @@ user = event.messageReply.senderID;
 } else { user = text;}
 	try {
 const avatar = `https://graph.facebook.com/${user}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`;
-const response = await a.get(avatar, {responseType: 'stream'});
+const response = await axios.get(avatar, {responseType: 'stream'});
 await api.sendMessage({attachment: response.data }, event.messageID, event.threadID);
 } catch (error) {
 api.sendMessage("❌ | " + error.message, event.threadID, event.messageID);
