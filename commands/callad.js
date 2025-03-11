468eda0c8ed8fd7ec374f868d58a068a563dab61
Tawsif <challengex46@gmail.com>
2025-03-11 22:39:25 +0600

    Update callad.js

--- commands/callad.js
+++ commands/callad.js
@@ -5,7 +5,7 @@ module.exports = {
         author: "Tawsif~",
         role: 0,
         shortDescription: "sends message to bot owner",
-        guide: "callad <text>\nfor owner: callad reply <tid> <text>"
+        guide: "callad <text>\nfor admin: callad reply <tid> <text>"
     },
     async execute({ api, event, args }) {
         try {
@@ -15,14 +15,14 @@ module.exports = {
             if (args[0] === "reply") {
                 if (event.senderID === "100063840894133") {
 			if (!tid || !amsg) { return api.sendMessage("invalid format. type help callad for guide", event.threadID);
-					   } else { api.sendMessage(`REPLY FROM TAWSIF:\n—————————————————\n${amsg}`, tid);
+					   } else { api.sendMessage(`📨REPLY FROM TAWSIF:📨\n———————————————————\n${amsg}`, tid);
 						  }
                 } else {
                     api.sendMessage("❌ | You do not have permission to reply.", event.threadID);
                 }
             } else if (!msg) { return api.sendMessage("text can't be empty", event.threadID);
 			     } else { const name = await api.getUserInfo(event.senderID);
-	api.sendMessage(`CALL ADMIN\nNAME: ${name[event.senderID].name}\nTHREAD ID: ${event.threadID}\nBODY: ${msg}`, 8664093487004091);
+	api.sendMessage(`📨CALL ADMIN📨\nNAME: ${name[event.senderID].name}\nTHREAD ID: ${event.threadID}\nBODY: ${msg}`, 8664093487004091);
 				     api.sendMessage("✅ | successfully sent your message to bot admin", event.threadID);
             }
         } catch (error) {
