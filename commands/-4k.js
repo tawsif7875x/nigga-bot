5b62f8a3e6efb7b24e4b8ad70eecfc1bdb283942
Tawsif <challengex46@gmail.com>
2025-03-17 05:21:57 +0600

    Create -4k.js

---
+++ commands/-4k.js
@@ -0,0 +1,21 @@
+const axios = require('axios');
+module.exports = {
+config: {
+	name: "-4k",
+	author: "Tawsif~",
+	role: 0,
+	guide: "-4k <reply>"
+},
+async execute({ api, event }) {
+const imageUrl = event.messageReply.attachments[0].thumbnailUrl;
+if (!event?.messageReply?.attachments[0]?.thumbnailUrl) { return api.sendMessage("reply to an image", event.threadID);
+api.setMessageReactionMqtt("⏳", event.messageID, event.threadID);
+setTimeout(() => {
+const response = await axios.get(imageUrl, {responseType: 'stream'});
+await api.sendMessage({body: "✅ | Here's your image✨", attachment: response.data }, event.threadID, event.messageID);
+}, 5000);
+} catch (error) {
+api.sendMessage("❌ | " + error.message, event.threadID, event.messageID);
+		}
+	}
+}
