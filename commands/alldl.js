23b3768b54f8d6dc9df94b82af9d4ba846aefe21
Tawsif <challengex46@gmail.com>
2025-03-17 17:17:58 +0600

    Update alldl.js

--- commands/alldl.js
+++ commands/alldl.js
@@ -14,7 +14,7 @@ if (!args[1]) { type = "sd";
 }
 let videoUrl;
 if (args[0]) { 
-if ((args[0]).match(/.com/)) { videoUrl = args[0];
+if ((args[0]).match(/com/)) { videoUrl = args[0];
 } else { return api.sendMessage("provide or reply to a valid URL", event.threadID);
 }
 } else if (event.messageReply) { videoUrl = event.messageReply.body;
@@ -22,7 +22,7 @@ if ((args[0]).match(/.com/)) { videoUrl = args[0];
 }
 api.setMessageReactionMqtt("⏳", event.messageID, event.threadID);
 const apiUrl = `https://smfahim.onrender.com/download?url=${videoUrl}`
-const response = (await axios.get(videoUrl))[type];
+const response = (await axios.get(apiUrl))[type];
 const v = await axios.get(response, {responseType: 'stream'});
 await api.sendMessage({attachment: v.data }, event.threadID, event.messageID);
 } catch (error) {
