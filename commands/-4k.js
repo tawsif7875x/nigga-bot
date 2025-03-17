d72361c1432ea47b68ae1145931b325879fadd29
Tawsif <challengex46@gmail.com>
2025-03-17 13:31:21 +0600

    Update -4k.js

--- commands/-4k.js
+++ commands/-4k.js
@@ -7,14 +7,16 @@ config: {
 	guide: "-4k <reply>"
 },
 async execute({ api, event }) {
-const imageUrl = event.messageReply.attachments[0].thumbnailUrl;
-if (!event?.messageReply?.attachments[0]?.thumbnailUrl) { return api.sendMessage("reply to an image", event.threadID);
+try {
+let imageUrl;
+if (event?.messageReply?.attachments[0]?.thumbnailUrl) { imageUrl = event.messageReply.attachments[0].thumbnailUrl;
+} else { return api.sendMessage("reply to an image", event.threadID);
 }
 api.setMessageReactionMqtt("⏳", event.messageID, event.threadID);
 setTimeout(() => {
 const response = await axios.get(imageUrl, {responseType: 'stream'});
-await api.sendMessage({body: "✅ | Here's your image✨", attachment: response.data }, event.threadID, event.messageID);
 }, 5000);
+await api.sendMessage({body: "✅ | Here's your image✨", attachment: response.data }, event.threadID, event.messageID);
 } catch (error) {
 api.sendMessage("❌ | " + error.message, event.threadID, event.messageID);
 		}
