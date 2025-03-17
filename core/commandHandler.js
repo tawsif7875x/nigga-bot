e0bd7a5f7672690c5f184450250c5e87c3784b21
Tawsif <challengex46@gmail.com>
2025-03-18 05:07:33 +0600

    Update commandHandler.js

--- core/commandHandler.js
+++ core/commandHandler.js
@@ -45,6 +45,56 @@ async function handleCommand(api, event) {
       return api.sendMessage("⚠️ You don't have permission to use this command.", threadID);
     }
   }
+  function message(api, event) {
+	async function sendMessageError(err) {
+		if (typeof err === "object" && !err.stack)
+			err = utils.removeHomeDir(JSON.stringify(err, null, 2));
+		else
+			err = utils.removeHomeDir(`${err.name || err.error}: ${err.message}`);
+		return await api.sendMessage(utils.getText("utils", "errorOccurred", err), event.threadID, event.messageID);
+	}
+	return {
+		send: async (form, callback) => {
+			try {
+				global.statusAccountBot = 'good';
+				return await api.sendMessage(form, event.threadID, callback);
+			}
+			catch (err) {
+				if (JSON.stringify(err).includes('spam')) {
+					setErrorUptime();
+					throw err;
+				}
+			}
+		},
+		reply: async (form, callback) => {
+			try {
+				global.statusAccountBot = 'good';
+				return await api.sendMessage(form, event.threadID, callback, event.messageID);
+			}
+			catch (err) {
+				if (JSON.stringify(err).includes('spam')) {
+					setErrorUptime();
+					throw err;
+				}
+			}
+		},
+		unsend: async (messageID, callback) => await api.unsendMessage(messageID, callback),
+		reaction: async (emoji, messageID, callback) => {
+			try {
+				global.statusAccountBot = 'good';
+				return await api.setMessageReaction(emoji, messageID, callback, true);
+			}
+			catch (err) {
+				if (JSON.stringify(err).includes('spam')) {
+					setErrorUptime();
+					throw err;
+				}
+			}
+		},
+		err: async (err) => await sendMessageError(err),
+		error: async (err) => await sendMessageError(err)
+	};
+}
 
   // Check cooldown
   const timestamps = cooldowns.get(command.config.name);
