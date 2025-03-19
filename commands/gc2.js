ef155242c7acee916c9ebf241d0fbf4ebc9413bc
Tawsif <challengex46@gmail.com>
2025-03-19 17:31:36 +0600

    Update gc2.js

--- commands/gc2.js
+++ commands/gc2.js
@@ -136,7 +136,7 @@ module.exports = {
 
     // Draw the avatar
     const avatarX = 30;
-    const avatarY = canvasHeight - 30;
+    const avatarY = canvasHeight - 80;
     const avatarWidth = 60;
     const avatarHeight = 60;
 
