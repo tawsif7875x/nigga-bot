906890df0e185d418db33ee929b26d6a007247a3
Tawsif <challengex46@gmail.com>
2025-03-22 17:32:35 +0600

    Update gc2.js

--- commands/gc2.js
+++ commands/gc2.js
@@ -57,8 +57,7 @@ module.exports = {
       }
     }
     let mentionedName = (await api.getUserInfo(mentionedID))[mentionedID].name;
-    let ThreadInfo = await api.getThreadInfo(event.threadID);
-    let background = ["https://raw.githubusercontent.com/tawsif7875x/nigga-bot/refs/heads/main/1742466954445.png"];
+    let background = ["https://raw.githubusercontent.com/tawsif7875x/nigga-bot/refs/heads/main/1742466954445.png", "https://raw.githubusercontent.com/tawsif7875x/nigga-bot/refs/heads/main/1742642829480.png"];
     let rd = background[0];
     let getAvtmot = (await axios.get(
       `https://graph.facebook.com/${mentionedID}/picture?width=720&height=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`,
