1f88154d4f54bc2d1d093659233c9601fda8fd32
Tawsif <challengex46@gmail.com>
2025-03-26 21:18:52 +0600

    Update gc2.js

--- commands/gc2.js
+++ commands/gc2.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const axios = require("axios");
 
 // Scale factor for high resolution (2x or 3x)
-const SCALE_FACTOR = 1;
+const SCALE_FACTOR = 5;
 
 module.exports = {
   config: {
